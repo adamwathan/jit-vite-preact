79422fa0099f348ba95d28e22ae01c428f96001f
Martin Oppitz <github@martinoppitz.com>
2021-03-19 00:14:57 +0100

    break

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
-import preactRefresh from '@prefresh/vite'
-import { defineConfig } from 'vite'
+const { resolve } = require("path");
+import { defineConfig } from 'vite';
+
+import preactRefresh from '@prefresh/vite';
 
 export default defineConfig({
+  css: {
+    // hear is the problem
+    postcss: require(resolve(process.cwd(), "postcss.config")),
+  },
   esbuild: {
     jsxFactory: 'h',
     jsxFragment: 'Fragment'
